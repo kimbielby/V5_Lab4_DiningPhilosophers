af51e46dd7bea6b7342e32ce275ac10aacc2ea22
kimbielby <kib00075@students.stir.ac.uk>
2021-10-15 12:32:17 +0100

    Added class PhilActions.

--- src/StartPhilosophising.java
+++ src/StartPhilosophising.java
@@ -1,4 +1,14 @@
 import java.util.concurrent.Semaphore;
 
 public class StartPhilosophising {
+
+    public static Semaphore chop1;
+    public static Semaphore chop2;
+    public static Semaphore chop3;
+
+    public StartPhilosophising(){
+        chop1 = new Semaphore(1);
+        chop2 = new Semaphore(1);
+        chop3 = new Semaphore(1);
+    }
 }
